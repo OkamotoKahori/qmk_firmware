2d05cea5ec637602ef505ec816d8f7905a07c77f
OkamotoKahori <kahori.n811@gmail.com>
2018-05-17 22:59:36 +0900

    キーマップの変更

--- keyboards/ergo42/keymaps/default/keymap.c
+++ keyboards/ergo42/keymaps/default/keymap.c
@@ -17,51 +17,51 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* BASE
  * ,------------------------------------------------.   ,------------------------------------------------.
- * | ESC  | Tab  |   Q  |   W  |   E  |   R  |  T   |   |  Y   |   U  |   I  |   O  |   P  |   [  | Bksp |
+ * | LAlt | Tab  |   Q  |   W  |   E  |   R  |  T   |   |  Y   |   U  |   I  |   O  |   P  |   [  | POWER|
  * |------+------+------+------+------+------+------|   |-------------+------+------+------+------+------|
- * | Del  | RCtrl|   A  |   S  |   D  |   F  |  G   |   |  H   |   J  |   K  |   L  |   ;  |   ]  | Enter|
+ * | Del  | RCtrl|   A  |   S  |   D  |   F  |  G   |   |  H   |   J  |   K  |   L  |   ;  |   ]  |  \   |
  * |------+------+------+------+------+------+------|   |------|------+------+------+------+------+------|
  * | SYMB | LSft |   Z  |   X  |   C  |   V  |  B   |   |  N   |   M  |   ,  |   .  |   /  |  UP  | RSft |
  * |------+------+------+------+------+------+------|   |------+------+------+------+------+------+------|
- * | META | LCtrl|   `  |   \  | LAlt | LGUI |Space |   |Space |   '  |   -  |   =  | LEFT | DOWN | RIGHT|
+ * | META | LCtrl|   `  |   '  | LGUI | ESC  |Space |   |Enter | Bksp |   -  |   =  | LEFT | DOWN | RIGHT|
  * `------------------------------------------------'   `------------------------------------------------'
  */
 
 [BASE] = KEYMAP( \
-  KC_ESC,   KC_TAB,  KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,   KC_Y,   KC_U,    KC_I,    KC_O,   KC_P,    KC_LBRC, KC_BSPC, \
-  KC_DELT,  KC_RCTL, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,   KC_H,   KC_J,    KC_K,    KC_L,   KC_SCLN, KC_RBRC, KC_ENT, \
+  KC_LALT,   KC_TAB,  KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,   KC_Y,   KC_U,    KC_I,    KC_O,   KC_P,    KC_LBRC,KC_SYSTEM_POWER, \
+  KC_DELT,  KC_RCTL, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,   KC_H,   KC_J,    KC_K,    KC_L,   KC_SCLN, KC_RBRC, KC_BSLS, \
   MO(SYMB), KC_LSFT, KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,   KC_N,   KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_UP,   KC_RSFT, \
-  MO(META), KC_LCTL, KC_GRV, KC_BSLS, KC_LALT, KC_LGUI, KC_SPC, KC_SPC, KC_QUOT, KC_MINS, KC_EQL, KC_LEFT, KC_DOWN, KC_RGHT \
+  MO(META), KC_LCTL, KC_GRV, KC_QUOT, KC_LGUI, KC_ESC, KC_SPC, KC_ENT, KC_BSPC, KC_MINS, KC_EQL, KC_LEFT, KC_DOWN, KC_RGHT \
 ),
 
 /* META
  * ,------------------------------------------------.   ,------------------------------------------------.
- * | Reset|   1  |   2  |   3  |   4  |   5  |  [   |   |  ]   |   6  |   7  |   8  |   9  |   0  | Bksp |
+ * | Reset|      |  F9  |  F10 |  F11 |  F12 |  [   |   |  ]   |      |   7  |   8  |   9  |      | POWER|
  * |------+------+------+------+------+------+------|   |-------------+------+------+------+------+------|
- * | Del  |  F1  |      |Muhen | Henk |      |      |   | Left | Down |  Up  |Right |      |      | Enter|
+ * | Del  |      |  F5  |  F6  |  F7  |  F8  |  (   |   |  )   |      |   4  |   5  |   6  |      |  \   |
  * |------+------+------+------+------+------+------|   |------|------+------+------+------+------+------|
- * | SYMB |  F2  |  F3  |  F4  |  F5  |  F6  |  {   |   |  }   |  F7  |  F8  |  F9  | F10  | F11  | F12  |
+ * | SYMB |      |  F1  |  F2  |  F3  |  F4  |  {   |   |  }   |   =  |   1  |   2  |   3  |  UP  | RSft |
  * |------+------+------+------+------+------+------|   |------+------+------+------+------+------+------|
- * | META | LCtrl|   `  |   \  | LAlt | LGUI |Space |   |Space |   '  |   -  |   =  |      |      |      |
+ * | META | LCtrl|   `  |   '  | LGUI | ESC  |Space |   |Enter | Bksp |   -  |   0  | LEFT | DOWN | RIGHT|
  * `------------------------------------------------'   `------------------------------------------------'
  */
 
 [META] = KEYMAP( \
-  RESET,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_LBRC,    KC_RBRC,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______, \
-  _______, KC_F1,   XXXXXXX, KC_MHEN, KC_HENK, XXXXXXX, XXXXXXX,    KC_LEFT,    KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, XXXXXXX, _______, \
-  _______, KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   S(KC_LBRC), S(KC_RBRC), KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, \
-  _______, _______, _______, _______, _______, _______, _______,    _______,    _______, _______, _______, XXXXXXX, XXXXXXX, XXXXXXX \
+  RESET,   XXXXXXX,   KC_F9,  KC_F10,  KC_F11,  KC_F12, KC_LBRC,    KC_RBRC,    XXXXXXX, KC_7,    KC_8,    KC_9,    XXXXXXX,    _______, \
+  _______, XXXXXXX,   KC_F5,   KC_F6,   KC_F7,   KC_F8, S(KC_9),    S(KC_0),    XXXXXXX, KC_4,    KC_5,    KC_6,    XXXXXXX, _______, \
+  _______, XXXXXXX,   KC_F1,   KC_F2,   KC_F3,   KC_F4, S(KC_LBRC), S(KC_RBRC), KC_EQL , KC_1,    KC_2,    KC_3,  _______,  _______, \
+  _______, _______, _______, _______, _______, _______, _______,    _______,    _______, _______, KC_0, _______, _______, _______ \
 ),
 
 /* SYMB
  * ,------------------------------------------------.   ,------------------------------------------------.
- * | ESC  |   !  |   @  |   #  |   $  |   %  |  [   |   |  ]   |   ^  |   &  |   *  |   (  |   )  | Bksp |
+ * | LAlt |   !  |   @  |   #  |   $  |   %  |  [   |   |  ]   |   ^  |   &  |   *  |   (  |   )  | POWER|
  * |------+------+------+------+------+------+------|   |-------------+------+------+------+------+------|
- * | Del  |      |      |      |      |      |  (   |   |  )   |      |      |      |      |      | Enter|
+ * | Del  |      |      |      |      |      |  (   |   |  )   |      |      |      |      |      |  \   |
  * |------+------+------+------+------+------+------|   |------|------+------+------+------+------+------|
  * | SYMB |      |      |      |      |      |  {   |   |  }   |      |      |      |      |  UP  | Sft  |
  * |------+------+------+------+------+------+------|   |------+------+------+------+------+------+------|
- * | META | LCtrl|   `  |   \  | LAlt | LGUI |Space |   |Space |   '  |   -  |   =  | LEFT | DOWN | RIGHT|
+ * | META | LCtrl|   `  |   '  | LGUI | ESC  |Space |   |Enter | Bksp |   -  |   =  | LEFT | DOWN | RIGHT|
  * `------------------------------------------------'   `------------------------------------------------'
  */
 
